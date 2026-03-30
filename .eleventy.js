1e1621ee4af60d4c4086849e9721143595b7f402
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-03-30 17:51:37 +0100

    add plugin

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function (eleventyConfig) {
 
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
     eleventyConfig.addPassthroughCopy('src/styles');
     eleventyConfig.addWatchTarget('src/styles');
     eleventyConfig.addPassthroughCopy('src/assets');
