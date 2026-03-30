857ef13b1db0f521671160d0ed4779621827b93e
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-03-30 18:59:29 +0100

    Add js to passthroughcopy

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/styles');
     eleventyConfig.addWatchTarget('src/styles');
     eleventyConfig.addPassthroughCopy('src/assets');
+    eleventyConfig.addPassthroughCopy("src/js");    
 
     eleventyConfig.addCollection('pages', (collection) => {
         return collection.getFilteredByGlob('src/pages/*.md');
